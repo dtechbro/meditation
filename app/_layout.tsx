7ba0c7e76b690567e298ea57529baaf505f70f8e
dtechbro <princeakinola05@gmail.com>
2024-10-29 23:56:24 +0100

    'learning

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,10 +2,10 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack>3
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="meditate" options={{ headerShown: false }} />
+      <Stack.Screen name="meditate/[id]" options={{ headerShown: false }} />
     </Stack>
   );
 }
