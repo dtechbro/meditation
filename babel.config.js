17ee5278b145f30df43d67249d4c1d3028b352b3
dtechbro <princeakinola05@gmail.com>
2024-11-26 22:25:32 +0100

    finale

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,11 @@
-const { default: plugin } = require("tailwindcss");
+// const { default: plugin } = require("tailwindcss");
 
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['nativewind/babel']
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      'nativewind/babel',
+    ]
   };
 };
